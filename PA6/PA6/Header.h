c2724f8c665720ef17c391f17776206621642bd0
Benjamin Poile <benp51@outlook.com>
2017-11-05 13:05:07 -0800

    A

--- PA6/PA6/Header.h
+++ PA6/PA6/Header.h
@@ -3,15 +3,17 @@
 
 int welcome_screen(int menu_selection);
 
-void play_game();
+void play_game(void);
 void initialize_board(char board[10][10]);
 char * manually_place_ships(char board[10][10]);
 char * randomly_place_ships(char board[10][10]);
 int check_shot(char board[10][10]);
 int check_if_ship_sunk(char board[10][10]);
 int is_winner(char board[10][10]);
+int ship_sunk(char board[10][10], char ship_code);
 void update_board(char board[10][10]);
 void display_board(char board[10][10]);
+
 
 
 void output_current_move(char board[10][10], char x, int y, int move_status);
